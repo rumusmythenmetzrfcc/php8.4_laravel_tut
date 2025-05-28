5f82f4530775d82ab2ab1b5a9e8a670ac8a4e0e0
Rumus Mythenmetz <rumus.mythenmetz.afrcc@gmail.com>
2025-05-28 20:00:23 +0200

    @ video tutorial time 01:02:00

---
+++ medium-app/database/factories/PostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+
+        return [
+            'image' => fake()->imageUrl(),
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => fake()->paragraph(5),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'user_id' => 1,
+            'published_at' => fake()->optional()->dateTime()
+        ];
+    }
+}
--- medium-app/database/seeders/DatabaseSeeder.php
+++ medium-app/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = [
+            'Technology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+            'Entertainment',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+            ]);
+        }
+
+        Post::factory(100)->create();
     }
 }
