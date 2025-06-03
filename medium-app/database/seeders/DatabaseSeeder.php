79137f7b22e690db89ca4f3af77d8b966d9aa559
Rumus Mythenmetz <rumus.mythenmetz.afrcc@gmail.com>
2025-06-03 17:49:13 +0200

    Creates PostController

--- medium-app/database/seeders/DatabaseSeeder.php
+++ medium-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@test.com',
+        ]);
+
         $categories = [
             'Technology',
             'Health',
